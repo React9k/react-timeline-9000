50625f182022b6fee8126ae8f8fe8cba90fc4eb2
Leighton Lilford <lrlilford@gmail.com>
2018-06-16 17:33:08 +0800

    Add prettier

--- src/test.js
+++ src/test.js
@@ -1,12 +1,11 @@
-'use strict';
-import {expect} from 'chai';
+"use strict";
+import { expect } from "chai";
 
-import texter from './'
+import texter from "./";
 
-
-describe('#texter', function() {
-    it('should return the same text', function() {
-        let result = texter("Hi")
+describe("#texter", function() {
+    it("should return the same text", function() {
+        let result = texter("Hi");
         expect(result).to.equal("Hi");
     });
 });
