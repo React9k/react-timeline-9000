c568a8c162269af175d09754dc65d55251e5f485
Leighton Lilford <lrlilford@gmail.com>
2018-07-10 08:46:11 +0800

    Make npm ready

---
+++ src/demo_index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import DemoTimeline from 'demo';
+
+ReactDOM.render(<DemoTimeline />, document.getElementById('root'));
