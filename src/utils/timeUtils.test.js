1977a84f6a23f8cc4736f6be0c73e7d028784ef4
Vlad Iordanescu <vlad.iordanescu@crispico.com>
2021-04-13 13:34:41 +0300

    - moved snap calculation in tests

--- src/utils/timeUtils.test.js
+++ src/utils/timeUtils.test.js
@@ -8,64 +8,64 @@ describe('Time Utils', function() {
     it('should round up to the last sec', function() {
       const testTime = moment('2000-01-01 10:00:00.872 Z', 'YYYY-MM-DD H:m:s.SSS Z');
       const expectedTime = moment('2000-01-01 10:00:00.000 Z', 'YYYY-MM-DD H:m:s.SSS Z');
-      const snap = 1;
-      const actualTime = timeSnap(testTime, snap * 1000);
+      const snap = 1000;
+      const actualTime = timeSnap(testTime, snap);
       expect(actualTime.unix()).to.equal(expectedTime.unix());
     });
     it('should round down to the last sec', function() {
       const testTime = moment('2000-01-01 10:00:00.272 Z', 'YYYY-MM-DD H:m:s.SSS Z');
       const expectedTime = moment('2000-01-01 10:00:00.000 Z', 'YYYY-MM-DD H:m:s.SSS Z');
-      const snap = 1;
-      const actualTime = timeSnap(testTime, snap * 1000);
+      const snap = 1000;
+      const actualTime = timeSnap(testTime, snap);
       expect(actualTime.unix()).to.equal(expectedTime.unix());
     });
     it('should round up to the nearest min', function() {
       const testTime = moment('2000-01-01 9:59:50 Z', 'YYYY-MM-DD H:m:s Z');
       const expectedTime = moment('2000-01-01 10:00:00 Z', 'YYYY-MM-DD H:m:s Z');
-      const snap = 60;
-      const actualTime = timeSnap(testTime, snap * 1000);
+      const snap = 60 * 1000;
+      const actualTime = timeSnap(testTime, snap);
       expect(actualTime.unix()).to.equal(expectedTime.unix());
     });
     it('should round down to the nearest min', function() {
       const testTime = moment('2000-01-01 10:00:20 Z', 'YYYY-MM-DD H:m:s Z');
       const expectedTime = moment('2000-01-01 10:00:00 Z', 'YYYY-MM-DD H:m:s Z');
-      const snap = 60;
-      const actualTime = timeSnap(testTime, snap * 1000);
+      const snap = 60 * 1000;
+      const actualTime = timeSnap(testTime, snap);
       expect(actualTime.unix()).to.equal(expectedTime.unix());
     });
     it('should round up when at 30s (nearest min)', function() {
       const testTime = moment('2000-01-01 10:00:30 Z', 'YYYY-MM-DD H:m:s Z');
       const expectedTime = moment('2000-01-01 10:01:00 Z', 'YYYY-MM-DD H:m:s Z');
-      const snap = 60;
-      const actualTime = timeSnap(testTime, snap * 1000);
+      const snap = 60 * 1000;
+      const actualTime = timeSnap(testTime, snap);
       expect(actualTime.unix()).to.equal(expectedTime.unix());
     });
     it('should round to nearest hour', function() {
       const testTime = moment('2000-01-01 10:12:30 Z', 'YYYY-MM-DD H:m:s Z');
       const expectedTime = moment('2000-01-01 10:00:00 Z', 'YYYY-MM-DD H:m:s Z');
-      const snap = 60 * 60;
-      const actualTime = timeSnap(testTime, snap * 1000);
+      const snap = 60 * 60 * 1000;
+      const actualTime = timeSnap(testTime, snap);
       expect(actualTime.unix()).to.equal(expectedTime.unix());
     });
     it('should round to nearest hour over mid-night', function() {
       const testTime = moment('2000-01-01 23:44:40 Z', 'YYYY-MM-DD H:m:s Z');
       const expectedTime = moment('2000-01-02 00:00:00 Z', 'YYYY-MM-DD H:m:s Z');
-      const snap = 60 * 60;
-      const actualTime = timeSnap(testTime, snap * 1000);
+      const snap = 60 * 60 * 1000;
+      const actualTime = timeSnap(testTime, snap);
       expect(actualTime.unix()).to.equal(expectedTime.unix());
     });
     it('should round up to nearest day', function() {
       const testTime = moment('2000-01-01 12:44:40 Z', 'YYYY-MM-DD H:m:s Z');
       const expectedTime = moment('2000-01-02 00:00:00 Z', 'YYYY-MM-DD H:m:s Z');
-      const snap = 60 * 60 * 24;
-      const actualTime = timeSnap(testTime, snap * 1000);
+      const snap = 60 * 60 * 24 * 1000;
+      const actualTime = timeSnap(testTime, snap);
       expect(actualTime.unix()).to.equal(expectedTime.unix());
     });
     it('should round down to nearest day', function() {
       const testTime = moment('2000-01-01 11:44:40 Z', 'YYYY-MM-DD H:m:s Z');
       const expectedTime = moment('2000-01-01 00:00:00 Z', 'YYYY-MM-DD H:m:s Z');
-      const snap = 60 * 60 * 24;
-      const actualTime = timeSnap(testTime, snap * 1000);
+      const snap = 60 * 60 * 24 * 1000;
+      const actualTime = timeSnap(testTime, snap);
       expect(actualTime.unix()).to.equal(expectedTime.unix());
     });
   });
