2fbe4cc8a0634c5ff652d557067c5ae7cea0dd16
Cristian Spiescu <cristian.spiescu@crispico.com>
2022-11-17 16:38:57 +0100

    Added types.js, needed because we have types.d.ts

---
+++ src/types.js
@@ -0,0 +1,5 @@
+/**
+ * This file is empty, but is needed, because we have types.d.ts. And we do import it from other files
+ * (needed by tsc when it does its thing to generate the .d.ts files). And if we do this, storybook also
+ * wants to see a "real" file.
+ */
